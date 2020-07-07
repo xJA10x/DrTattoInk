e104d0a512c91fff8c5ae14eb8404658b982d5c9
jhoset10 <jhosetceron@aol.com>
2020-07-07 18:17:05 -0400

    New code

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>D.R.Tatto Ink</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header id="top" class="main-header">
+      <?php include("include/header.html"); ?>
+    </header>
+    <div class="primary-content">
+      <div id="gallery">
+        <?php include("gallery.html"); ?>
+      </div>
+      <div id="contact">
+        <?php include("contact.html"); ?>
+      </div>
+    </div>
+    <footer class="main-footer">
+     <?php include("include/footer.html"); ?>
+    </footer>
+  </body>
+</html>
