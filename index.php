332237002f4fa91c4be1253c2a8fd7f7066a40a7
jhoset10 <jhosetceron@aol.com>
2020-07-07 20:31:56 -0400

    Create hamburger menu

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
   <head>
     <title>D.R.Tatto Ink</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <header id="top" class="main-header">
       <!-- How to include files in php -->
+       <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
       <?php include("include/header.html"); ?>
     </header>
     <div class="primary-content">
